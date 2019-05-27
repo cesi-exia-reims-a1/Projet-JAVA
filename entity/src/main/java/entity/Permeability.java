bcf869bc7d6b5957c1dd88e0bf4e540432d8b60f
Romain MALARMEY <romain.malarmey@viacesi.fr>
2019-05-27 12:56:38 +0200

    Remise à Zéros and try again

--- entity/src/main/java/entity/Permeability.java
+++
@@ -1,7 +0,0 @@
-package entity;
-
-public enum Permeability {
-
-	BLOCKING,
-	PENETRABLE;
-}
--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -4,11 +4,6 @@
  */
 package main;
 
-import contract.ControllerOrder;
-import controller.Controller;
-import model.Model;
-import view.View;
-
 /**
  * The Class Main.
  *
@@ -22,11 +17,6 @@ public abstract class Main {
 	 * @param args the arguments
 	 */
 	public static void main(final String[] args) {
-		final Model model = new Model();
-		final View view = new View(model);
-		final Controller controller = new Controller(view, model);
-		view.setController(controller);
-		controller.control();
-		controller.orderPerform(ControllerOrder.English);
+
 	}
 }
