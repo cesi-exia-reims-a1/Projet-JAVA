1d7ec7d196613b649518c053ef2c3c7fd4549a5c
Romain MALARMEY <romain.malarmey@viacesi.fr>
2019-05-27 10:36:13 +0200

    Correction d'erreur

--- entity/src/main/java/entity/Mobile.java
+++ entity/src/main/java/entity/Mobile.java
@@ -4,6 +4,11 @@ import java.awt.Point;
 
 public abstract class Mobile extends Entity {
 
+	public Mobile(Sprite sprite, Permeability permeability) {
+		super(sprite, permeability);
+		// TODO Auto-generated constructor stub
+	}
+
 	private Point position;
 
 	private Boolean alive = true;
--- entity/src/main/java/entity/MotionLess.java
+++ entity/src/main/java/entity/MotionLess.java
@@ -4,6 +4,11 @@ import java.awt.Point;
 
 public abstract class MotionLess extends Entity {
 
+	public MotionLess(Sprite sprite, Permeability permeability) {
+		super(sprite, permeability);
+		// TODO Auto-generated constructor stub
+	}
+
 	private Point position;
 
 	private Boolean alive = true;
