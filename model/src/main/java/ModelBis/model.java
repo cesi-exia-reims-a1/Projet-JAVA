15345afe9d78b98160bf10142722552d726fcd28
Romain MALARMEY <romain.malarmey@viacesi.fr>
2019-05-29 09:42:58 +0200

    Model connecté à la BDD

--- model/src/main/java/ModelBis/model.java
+++ model/src/main/java/ModelBis/model.java
@@ -1,5 +1,25 @@
 package ModelBis;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class model {
 
+	public static void main(String[] args) {
+		try {
+
+			Class.forName("org.gjt.mm.mysql.Driver");
+			System.out.println("Driver O.K.");
+
+			String url = "jdbc:mysql://localhost:3306/jpublankproject";
+			String user = "root";
+			String passwd = "";
+
+			Connection conn = DriverManager.getConnection(url, user, passwd);
+			System.out.println("Connexion effective !");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
