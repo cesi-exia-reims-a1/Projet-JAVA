e714e689a2edb85048dca8bc4839067cffcd64b1
Vivien <vivintouvier@gmail.com>
2019-06-03 17:21:36 +0200

    ControllerTest (pas fonctionnel)

--- controller/src/main/java/controller/ControllerTest.java
+++ controller/src/main/java/controller/ControllerTest.java
@@ -2,38 +2,32 @@
 
 import static org.junit.Assert.*;
 
+import java.awt.Point;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import contract.Constants;
+import contract.IMap;
+import controller.Controller;
+
 public class ControllerTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
+	IMap map;
+	int[][] cases = map.getCases();
 
 	@Test
-	public void testMoveRight() {
-		final int expected = 3;
+	public void appliqueMovement(int x1, int y1, int x2, int y2){
+		try {
+			cases[y2][x2] = Constants.WALL;
+			//map.moveRight();	
+		} catch (Exception e) {
+			final String expected = "Index 20 out of bounds for length 20";
+			assertEquals(expected, e.getMessage());
+		}
 	}
 
-	@Test
-	public void testMoveLeft() {
-		fail("Not yet implemented");
-	}
 
-	@Test
-	public void testMoveUp() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testMoveDown() {
-		fail("Not yet implemented");
-	}
 
 }
