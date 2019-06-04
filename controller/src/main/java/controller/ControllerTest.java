679ae35114e146aab4bf4529cd386cedae2922d4
Vivien <vivintouvier@gmail.com>
2019-06-04 11:12:54 +0200

    test mouvement (pas fonctionnel)

--- controller/src/main/java/controller/ControllerTest.java
+++ controller/src/main/java/controller/ControllerTest.java
@@ -3,27 +3,56 @@
 import static org.junit.Assert.*;
 
 import java.awt.Point;
-
-import org.junit.After;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import org.junit.Before;
 import org.junit.Test;
 
-import contract.Constants;
 import contract.IMap;
 import controller.Controller;
 
 public class ControllerTest {
 
 	IMap map;
-	int[][] cases = map.getCases();
+	Controller controller = new Controller();
+	
+	@Before
+	public void loadMap() {
+		int[][] cases = new int[20][20];
+		map.setCases(cases);
+		try {
+
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver O.K.");
+
+			String url = "jdbc:mysql://localhost:3306/jpublankproject?serverTimezone=UTC";
+			String user = "root";
+			String passwd = "";
+
+			Connection conn = DriverManager.getConnection(url, user, passwd);
+			System.out.println("Effective connection!");
+
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM map1");
+
+			while (rs.next()) {
+				cases[rs.getInt("x")][rs.getInt("y")] = rs.getInt("id");
+			}
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	@Test
-	public void appliqueMovement(int x1, int y1, int x2, int y2){
+	public void TestmoveLeft(){
 		try {
-			cases[y2][x2] = Constants.WALL;
-			//map.moveRight();	
+			Point positionJoueur = controller.findPlayer();
+			controller.appliqueMovement(positionJoueur.x, positionJoueur.y, positionJoueur.x - 1, positionJoueur.y);
+			fail("Impossible");
 		} catch (Exception e) {
-			final String expected = "Index 20 out of bounds for length 20";
+			final String expected = "Impossible";
 			assertEquals(expected, e.getMessage());
 		}
 	}
